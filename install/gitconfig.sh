c1903aaf584f639f8050c4f0f86d91cb1ef7af1f
Sam Roquitte <samroq@yahoo.com>
2018-06-28 12:01:04 -0500

    Gitconfig install script

---
+++ install/gitconfig.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+printf "Downloading gitconfig from github...\n"
+cd ~/
+wget https://raw.githubusercontent.com/samr28/setup-files/master/gitconfig
+printf "====================\n"
+printf "Your gitconfig:\n\n"
+cat .gitconfig
+printf "==========\n"
+printf "Will be replaced with:\n\n"
+cat gitconfig
+printf "====================\n"
+
+while true; do
+    read -p "Do you want to replace your gitconfig? " yn
+    case $yn in
+        [Yy]* ) install; break;;
+        [Nn]* ) noinstall; break;;
+        * ) printf "Please answer yes or no.\n";;
+    esac
+done
+
+install() {
+  mv gitconfig .gitconfig
+  printf "Done!\n"
+}
+
+noinstall() {
+  rm gitconfig
+  printf "Ok bye!\n"
+}
